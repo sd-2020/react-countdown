d4994ec60b92bfeba6fbbf86a527693c8a0949ac
Dheeraj Sharma <sha.dhe0@gmail.com>
2022-03-31 00:20:46 +0530

    Adding initial stylings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Countdown from "./Countdown.js";
-import logo from "./logo.svg";
-import github from "./github.png";
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
@@ -10,26 +9,44 @@ class App extends Component {
       currentDate.getMonth() === 11 && currentDate.getDate() > 23
         ? currentDate.getFullYear() + 1
         : currentDate.getFullYear();
+    const count = [ ...Array(100).keys() ];
+
+    function getRandomArbitrary(min, max) {
+      return Math.random() * (max - min) + min;
+  }
+
     return (
       <div className="App">
+        <div class="background">
+          {
+            count.map(e => {return(<span style={{
+              color: `rgb(${getRandomArbitrary(0, 255)},${getRandomArbitrary(0, 255)},${getRandomArbitrary(0, 255)},0.2)`,
+              top: `${getRandomArbitrary(0,100)}%`,
+              left: `${getRandomArbitrary(0,100)}%`,
+              animationDuration: `${getRandomArbitrary(10,30)}s`,
+              animationDelay: `${getRandomArbitrary(0.1,0.3)}s`,
+              transformOrigin: `${getRandomArbitrary(5,95)}% ${getRandomArbitrary(5,95)}%`,
+              boxShadow: `${getRandomArbitrary(13,15)}vmin 0 4.302147264608503vmin`,
+            }}></span>)})
+          }
+
+
+        </div>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React Countdown</h2>
+          <h2>PHOTOSHOOTO</h2>
+          <span style={{ color: "black" }}>Your Memories, Our Promise.</span>
 
-          <a
-            href="https://github.com/kristinbaumann/react-countdown"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={github} alt="github" className="githubIcon" />
-            <span>View on Github</span>
-          </a>
         </div>
 
+
+        <Countdown date={`2022-04-05T00:00:00`} />
         <h3 className="title">
-          Christmas Eve is coming soon (Midnight of 23rd to 24th Dec, UTC time):
+          We will be live soon
         </h3>
-        <Countdown date={`${year}-12-24T00:00:00`} />
+        Siddayya Swamy
+
+
       </div>
     );
   }
